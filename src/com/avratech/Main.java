83a47d78a545c098cc910f2ba9c571a1d72ca41f
moshesl <moshe.slavin34@gmail.com>
2017-02-01 17:37:02 +0200

    wednesday

--- src/com/avratech/Main.java
+++ src/com/avratech/Main.java
@@ -1,13 +1,16 @@
 package com.avratech;
 
+import java.util.NavigableMap;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         //Mall m = new Mall("Kobe's mall", new Owner(), new Location(City.JERUSALEM, "", 1));
-    RestaurantShop m = new RestaurantShop(1,1,1,1,5,5,);
-
-
+        RestaurantShop shop1  = new RestaurantShop(1.0,1.0,1.0,1.0,1, 2);
+        RestaurantShop shop2  = new RestaurantShop(1.0,1.0,1.0,1.0,3, 4);
+        RestaurantShop shop3  = new RestaurantShop(1.0,1.0,1.0,1.0,5, 6);
+        RestaurantShop shop4  = new RestaurantShop(1.0,1.0,1.0,1.0,7, 8);
 
     }
 }
